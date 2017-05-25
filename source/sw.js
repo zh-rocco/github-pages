df97fd50e7abe85b682cfe9d0adb7aa2906bb85b
Simple <simpliberty@gmail.com>
2017-05-25 18:16:04 +0800

    优化PWA

--- source/sw.js
+++ source/sw.js
@@ -1,103 +1,34 @@
 'use strict';
 
-const version = 'simple_201705230000';
-const __DEVELOPMENT__ = false;
-const __DEBUG__ = true;
-const offlineResources = [
+let VERSION = 'v6';
+let OFFLINE_CACHE = 's-offline-' + VERSION;
+let DATA_CACHE = 's-data-' + VERSION;
+
+// 需要缓存的离线页面
+const FILES_TO_CACHE = [
   '/',
   '/offline.html',
-  '/images/avatar.jpg'
+  '/images/offline-image.png'
 ];
 
-const ignoreCache = [
+// 不需要缓存的地址
+const IGNORE_FILES = [
   /https?:\/\/hm.baidu.com\//,
-  /https?:\/\/cdn.bootcss.com\//,
-  /https?:\/\/static.duoshuo.com\//,
-  /https?:\/\/www.google-analytics.com\//,
-  /https?:\/\/dn-lbstatics.qbox.me\//,
-  /https?:\/\/ajax.cloudflare.com\//,
-  /https?:\/\/cdn1.lncld.net\//,
-  /https?:\/\/api.leancloud.cn\//,
-  /https?:\/\/lzw.me\/wp\-admin/,
+  /https?:\/\/www.google-analytics.com\//
 ];
-
-// 慎重使用全局可变变量，因为 serviceWork 不可控的停止和重启，会导致它们的取值在后续读取时无法预测
-let port;
-
 
 /**
- * common function
+ * 通用函数
  */
-
-function developmentMode() {
-  return __DEVELOPMENT__ || __DEBUG__;
-}
-
-function cacheKey() {
-  return [version, ...arguments].join(':');
-}
-
-function log() {
-  if (developmentMode()) {
-    console.log("SW:", ...arguments);
-  }
-}
 
 // 不需要缓存的请求
 function shouldAlwaysFetch(request) {
-  return __DEVELOPMENT__ ||
-    request.method !== 'GET' ||
-    ignoreCache.some(regex => request.url.match(regex));
+  return request.method !== 'GET' || IGNORE_FILES.some(regex => request.url.match(regex));
 }
 
-// 缓存 html 页面
+// 需要缓存的请求，例如：HTML页面
 function shouldFetchAndCache(request) {
   return (/text\/html/i).test(request.headers.get('Accept'));
-}
-
-// 发送 Notification 通知
-function sendNotify(title, options, event) {
-  if (Notification.permission !== 'granted') {
-    log('Not granted Notification permission.');
-
-    // 无授权时，向来源页面申请授权
-    if (port && port.postMessage) {
-      port.postMessage({
-        type: 'applyNotify',
-        info: {title, options}
-      });
-    }
-
-    return;
-  }
-
-  const notificationPromise = self.registration.showNotification(title || 'Hi：', Object.assign({
-    body: '这是一个通知示例',
-    icon: '//lzw.me/images/avatar/lzwme-80x80.png',
-    tag: 'push'
-  }, options));
-
-  return event && event.waitUntil(notificationPromise);
-}
-
-/**
- * onClickNotify
- */
-
-function onClickNotify(event) {
-  event.notification.close();
-  const url = "https://lzw.me";
-
-  event.waitUntil(
-    self.clients.matchAll({
-      type: "window"
-    })
-      .then(() => {
-        if (self.clients.openWindow) {
-          return self.clients.openWindow(url);
-        }
-      })
-  );
 }
 
 /**
@@ -105,12 +36,12 @@ function onClickNotify(event) {
  */
 
 function onInstall(event) {
-  log('install event in progress.');
+  console.log('[SW]:', '开始安装 Service Worker');
 
   event.waitUntil(
-    caches.open(cacheKey('offline'))
-      .then(cache => cache.addAll(offlineResources))
-      .then(() => log('installation complete! version: ' + version))
+    caches.open(OFFLINE_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => console.log('[SW]:', '离线资源缓存完毕，当前版本:', OFFLINE_CACHE))
       .then(() => self.skipWaiting())
   );
 }
@@ -121,9 +52,9 @@ function onInstall(event) {
 
 // 当网络离线或请求发生了错误，使用离线资源替代 request 请求
 function offlineResponse(request) {
-  log('(offline)', request.method, request.url);
+  console.log('[SW]:', '离线模式', request.method, request.url);
   if (request.url.match(/\.(jpg|png|gif|svg|jpeg)(\?.*)?$/)) {
-    return caches.match('/images/avatar.jpg');
+    return caches.match('/images/offline-image.png');
   } else {
     return caches.match('/offline.html');
   }
@@ -142,12 +73,12 @@ function networkedAndCache(request) {
     .then(response => {
       const copy = response.clone();
 
-      caches.open(cacheKey('resources'))
+      caches.open(DATA_CACHE)
         .then(cache => {
           cache.put(request, copy);
         });
 
-      log("(network: cache write)", request.method, request.url);
+      console.log('[SW]:', '写入缓存', request.method, request.url);
       return response;
     });
 }
@@ -156,7 +87,7 @@ function networkedAndCache(request) {
 function cachedOrNetworked(request) {
   return caches.match(request)
     .then((response) => {
-      log(response ? '(cached)' : '(network: cache miss)', request.method, request.url);
+      console.log('[SW]:', response ? '读取缓存' : '未匹配缓存', request.method, request.url);
       return response ||
         networkedAndCache(request)
           .catch(() => offlineResponse(request));
@@ -167,7 +98,7 @@ function cachedOrNetworked(request) {
 function networkedOrOffline(request) {
   return fetch(request)
     .then(response => {
-      log('(network)', request.method, request.url);
+      console.log('[SW]:', '网络请求', request.method, request.url);
       return response;
     })
     .catch(() => offlineResponse(request));
@@ -179,7 +110,7 @@ function onFetch(event) {
   // 应当永远从网络请求的资源
   // 如果请求失败，则使用离线资源替代
   if (shouldAlwaysFetch(request)) {
-    log('AlwaysFetch request: ', event.request.url);
+    console.log('[SW]:', 'AlwaysFetch request: ', event.request.url);
     event.respondWith(networkedOrOffline(request));
     return;
   }
@@ -206,17 +137,17 @@ function removeOldCache() {
     .then(keys =>
       Promise.all( // 等待所有旧的资源都清理完成
         keys
-          .filter(key => !key.startsWith(version)) // 过滤不需要删除的资源
-          .map(key => caches.delete(key)) // 删除旧版本资源，返回为 Promise 对象
+          .filter(key => key !== OFFLINE_CACHE && key !== DATA_CACHE) // 过滤不需要删除的资源
+          .map(key => {
+            console.log('[SW]:', '移除过时缓存:', key);
+            caches.delete(key);
+          }) // 删除旧版本资源，返回为 Promise 对象
       )
     )
-    .then(() => {
-      log('removeOldCache completed.');
-    });
 }
 
 function onActivate(event) {
-  log('activate event in progress.');
+  console.log('[SW]:', '激活 Service Worker');
   event.waitUntil(Promise.all([
     // 更新客户端
     self.clients.claim(),
@@ -224,56 +155,6 @@ function onActivate(event) {
   ]))
 }
 
-/**
- * onPush
- */
-
-function onPush(event) {
-  log('onPush ', event);
-  sendNotify('Hi:', {
-    body: `【${new Date()}】发生了一次 Push 同步事件 ~`
-  }, event);
-}
-
-/**
- * onSync
- */
-
-function onSync(event) {
-  log('onSync', event);
-  sendNotify('Hi:', {
-    body: `【${new Date()}】发生了一次 Sync 同步事件 ~`
-  }, event);
-}
-
-/**
- * onMessage
- */
-
-function onMessage(event) {
-  log('onMessage', event);
-
-  if (event.ports) {
-    port = event.ports[0];
-  }
-
-  if (!event.data) {
-    return;
-  }
-
-  // 如果是要求一条通知，则发送
-  if (event.data.type === 'notify') {
-    const {title, options} = event.data.info || {};
-    sendNotify(title, options, event);
-  }
-}
-
-log("Hello from ServiceWorker land!", version);
-
 self.addEventListener('install', onInstall);
 self.addEventListener('fetch', onFetch);
 self.addEventListener("activate", onActivate);
-self.addEventListener("push", onPush);
-self.addEventListener("sync", onSync);
-self.addEventListener('message', onMessage);
-self.addEventListener("notificationclick", onClickNotify);
